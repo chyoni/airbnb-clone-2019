de80e40a0f16a77c79cd43e6de214f4d12836f12
Chiwon <chiwon99881@gmail.com>
2019-11-12 18:46:17 +0900

    #5 user fields

--- users/models.py
+++ users/models.py
@@ -5,5 +5,23 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User Model """
+
+    # 변수 선언
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    # CharField 는 글자수 제한에 창 하나 TextField는 Textarea 같은거
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
 
