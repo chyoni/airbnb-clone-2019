81a5f73b8508ae91b17294013f5a7309b079565c
Chiwon <chiwon99881@gmail.com>
2019-11-18 17:21:59 +0900

    #24 add manyTomany fields (amenities,facilities,houseRules)

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -23,6 +23,10 @@ class Command(BaseCommand):
         seeder = Seed.seeder()
         all_users = user_models.User.objects.all()
         room_types = room_models.RoomType.objects.all()
+        amenities = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        house_rules = room_models.HouseRule.objects.all()
+
         seeder.add_entity(
             room_models.Room,
             number,
@@ -37,9 +41,11 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
+        # seeder를 사용해서 만들어진 레코드의 pk를 얻음
         created_room = seeder.execute()
         created_flatten = flatten(list(created_room.values()))
 
+        # 그 pk를 통해서 해당 룸에 대한 레코드를 찾고 그 룸에 photo들을 생성해 집어넣는 for 문
         for pk in created_flatten:
             room = room_models.Room.objects.get(pk=pk)
             for i in range(3, random.randint(10, 15)):
@@ -48,5 +54,17 @@ class Command(BaseCommand):
                     room=room,
                     file=f"room_photos/{random.randint(1,31)}.webp",
                 )
+            for a in amenities:
+                random_number = random.randint(0, 15)
+                if random_number % 2 == 0:
+                    room.amenities.add(a)
+            for f in facilities:
+                random_number = random.randint(0, 15)
+                if random_number % 2 == 0:
+                    room.facilities.add(f)
+            for h in house_rules:
+                random_number = random.randint(0, 15)
+                if random_number % 2 == 0:
+                    room.house_rules.add(h)
 
         self.stdout.write(self.style.SUCCESS(f"{number} rooms create !"))
