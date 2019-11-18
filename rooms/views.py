656ef5bdad8d37101d0fa8164b40f52f73b06bcb
Chiwon <chiwon99881@gmail.com>
2019-11-18 20:34:09 +0900

    #28 django template appetizer

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+
+def all_rooms(request):
+
+    now = datetime.now()
+    hungry = True
+
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
