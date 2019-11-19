0fb3f0ac20fdb1ee1946ccfcc14e4a57605dbdf0
Chiwon <chiwon99881@gmail.com>
2019-11-19 15:55:17 +0900

    #34 not get_page but page

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from rooms import models as room_models
 
 # Create your views here.
@@ -9,15 +9,19 @@ def all_rooms(request):
 
     page = request.GET.get("page", 1)
 
-    page = int(page or 1)
-
-    if page == 0:
-        page = 1
-
     # 이건 데이터베이스에서 데이터를 가져오는 것이 아니라 쿼리문을 생성한 것 뿐
     all_rooms = room_models.Room.objects.all()
 
-    paginator = Paginator(all_rooms, 10)
-    contacts = paginator.get_page(page)
+    # 이렇게 all_rooms를 호출하여야만 데이터베이스에서 데이터를 뽑아옴
+    # Django QuerySet is Lazy ! OK?
+    paginator = Paginator(all_rooms, 10, orphans=3)
 
-    return render(request, "rooms/home.html", context={"contacts": contacts},)
+    try:
+
+        contacts = paginator.page(int(page))
+
+        return render(request, "rooms/home.html", context={"contacts": contacts},)
+
+    except EmptyPage:
+
+        return redirect("/")
