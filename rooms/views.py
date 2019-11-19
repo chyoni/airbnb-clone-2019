b829e66b0d5c93206a8f476970f36cf4b807cc3a
Chiwon <chiwon99881@gmail.com>
2019-11-19 13:39:50 +0900

    #31 limit room data

--- rooms/views.py
+++ rooms/views.py
@@ -6,6 +6,15 @@ from rooms import models as room_models
 
 def all_rooms(request):
 
-    all_rooms = room_models.Room.objects.all()
+    print(request.GET.get("page"))
+    page = int(request.GET.get("page", 1))
+
+    if page == 0:
+        page = 1
+    room_size = 10
+    limit = room_size * page
+    offset = limit - room_size
+
+    all_rooms = room_models.Room.objects.all()[offset:limit]
 
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
