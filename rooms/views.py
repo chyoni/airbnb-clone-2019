eca719469904493321ca468210da7f5ccb871397
Chiwon <chiwon99881@gmail.com>
2019-11-19 18:43:57 +0900

    #36 room detail view

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils import timezone
 from . import models
 
 
@@ -9,3 +12,29 @@ class HomeView(ListView):
     model = models.Room
     paginate_by = 10
     paginate_orphans = 3
+    context_object_name = "rooms"
+
+    # context를 custom 할 수 있다
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        now = timezone.now()
+
+        context["now"] = now
+
+        return context
+
+
+def room_detail(request, pk):
+
+    try:
+
+        room = models.Room.objects.get(pk=pk)
+
+        return render(request, "rooms/detail.html", {"room": room})
+
+    except models.Room.DoesNotExist:
+
+        return redirect(reverse("core:home"))
+
