f5cc115b7ac86588c117f7ef6ff68f9615609d09
Chiwon <chiwon99881@gmail.com>
2019-11-19 15:03:11 +0900

    #32 previous and next button for passive

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from math import ceil
 from rooms import models as room_models
 
 # Create your views here.
@@ -6,15 +7,30 @@ from rooms import models as room_models
 
 def all_rooms(request):
 
-    print(request.GET.get("page"))
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+
+    page = int(page or 1)
 
     if page == 0:
         page = 1
+
     room_size = 10
+
     limit = room_size * page
+
     offset = limit - room_size
 
     all_rooms = room_models.Room.objects.all()[offset:limit]
 
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    page_count = ceil(room_models.Room.objects.count() / room_size)
+
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
