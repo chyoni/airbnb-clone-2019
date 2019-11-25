956cf242b272d9aa50309a8cea549dcc9da5346f
Chiwon <chiwon99881@gmail.com>
2019-11-25 13:06:24 +0900

    #40 search Part 2(remember my pick)

--- rooms/views.py
+++ rooms/views.py
@@ -40,10 +40,14 @@ def search(request):
 
     city = str.capitalize(city)
 
+    country = request.GET.get("country", "KR")
+
+    room_type = int(request.GET.get("room_type", 0))
+
     room_types = models.RoomType.objects.all()
 
-    return render(
-        request,
-        "rooms/search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
-    )
+    form = {"city": city, "s_country": country, "s_room_type": room_type}
+
+    choices = {"countries": countries, "room_types": room_types}
+
+    return render(request, "rooms/search.html", {**form, **choices},)
