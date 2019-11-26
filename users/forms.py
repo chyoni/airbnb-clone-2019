237404d9656996410877471ad457fdf6c2743916
Chiwon <chiwon99881@gmail.com>
2019-11-26 18:28:25 +0900

    #49 user login logout

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,28 @@
 from django import forms
+from . import models
 
 
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    # clean_xxx 처럼 특정 필드의 유효검사를 할때는 에러 메세지는 해당 필드에서 나오지만
+    # clean method는 필드간 서로 연관성이 있을때 한꺼번에 같이 검사하는 이런 메소드는
+    # 해당 에러가 어디서 생기는건지 따로 저렇게 self.add_error(field) 저 field 부분에 알려줘야함
+
+    def clean(self):
+
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Password is wrong."))
+
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("User does not exist."))
+
--- users/views.py
+++ users/views.py
@@ -1,15 +1,43 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
 class LoginView(View):
     def get(self, request):
 
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "admin@admin.com"})
 
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
+
         form = forms.LoginForm(request.POST)
-        print(form)
+
+        # form으로 보낸 데이터들이 유효한지 확인을 해야함 즉, 유저가 로그인할 때 아이디와 패스워드가
+        # 유효한지 확인을 한다는 얘기임
+        # 그래서 form(LoginForm 을 의미)에서 데이터를 확인하는 method가 있음 그게 clean_xxx method임
+        # 저기 xxx 는 유효한지 확인할 필드의 이름을 넣으면 됨
+        # clean method는 확인을해서 return을 해줌
+        # clean method가 return을 하지않으면 해당 필드의 데이터를 지워버림 그래서 return을 해줘야함
+
+        # 결국 결론은 post를 하게되면 저기 form에 가서 데이터를 확인하고 다시 돌려준다는 얘기임
+        # 돌려주면 여기서 확인을 함 is_valid로 확인을 하면 당연히 로그인을 시켜주겠지?
+
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+
+        return render(request, "users/login.html", {"form": form})
+
+
+def log_out(request):
+
+    logout(request)
+
+    return redirect(reverse("core:home"))
