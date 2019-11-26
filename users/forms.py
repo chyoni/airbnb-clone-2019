a05e81f6c5d13c76c3cf268304879340ee529ee0
Chiwon <chiwon99881@gmail.com>
2019-11-26 19:17:24 +0900

    #51 signup view

--- users/forms.py
+++ users/forms.py
@@ -26,3 +26,44 @@ class LoginForm(forms.Form):
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist."))
 
+
+class SignUpForm(forms.Form):
+
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    def clean_email(self):
+
+        email = self.cleaned_data.get("email")
+
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exist with that email.")
+        except models.User.DoesNotExist:
+            return email
+
+    # 장고가 위에서부터 하나씩 clean해서 cleaned_data 에 데이터를 넣기때문에
+    # 만약 clean_password 에서 password2 를 가져오려한다면 가져오지 못할 것
+    def clean_password2(self):
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+
+        if password != password2:
+            raise forms.ValidationError("Password confirmation does not match.")
+        else:
+            return password
+
+    def save(self):
+
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        user = models.User.objects.create_user(email, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
