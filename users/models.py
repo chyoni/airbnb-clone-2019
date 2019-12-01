f783014b44d74e1b76cd23be6f0b26f844a7d174
Chiwon <chiwon99881@gmail.com>
2019-12-01 17:23:15 +0900

    #56 github login Part 1

--- users/models.py
+++ users/models.py
@@ -39,6 +39,16 @@ class User(AbstractUser):
         (CURRENCY_KRW, "KRW"),
     )
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = (
+        (LOGIN_EMAIL, "Email"),
+        (LOGIN_GITHUB, "Github"),
+        (LOGIN_KAKAO, "Kakao"),
+    )
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     # CharField 는 글자수 제한에 창 하나 TextField는 Textarea 같은거
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
@@ -49,6 +59,9 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_confirmed = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=120, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
+    )
 
     def verify_email(self):
         if self.email_confirmed is False:
