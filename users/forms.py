1f02782bd2f12c55d66999f5005a1e69f0c12566
Chiwon <chiwon99881@gmail.com>
2019-12-01 12:50:31 +0900

    #52 transfer form To ModelForm

--- users/forms.py
+++ users/forms.py
@@ -27,26 +27,17 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist."))
 
 
-class SignUpForm(forms.Form):
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
 
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
-    def clean_email(self):
-
-        email = self.cleaned_data.get("email")
-
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exist with that email.")
-        except models.User.DoesNotExist:
-            return email
-
     # 장고가 위에서부터 하나씩 clean해서 cleaned_data 에 데이터를 넣기때문에
     # 만약 clean_password 에서 password2 를 가져오려한다면 가져오지 못할 것
+
     def clean_password2(self):
         password = self.cleaned_data.get("password")
         password2 = self.cleaned_data.get("password2")
@@ -56,14 +47,10 @@ class SignUpForm(forms.Form):
         else:
             return password
 
-    def save(self):
-
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
+    def save(self, *args, **kwargs):
+        user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
+        user.username = email
+        user.set_password(password)
         user.save()
